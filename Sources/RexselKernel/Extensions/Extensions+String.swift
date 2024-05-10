474559e217f53b1ec33e8b84525b50ed47e3c794
Hugh Field-Richards <hsfr@hsfr.org.uk>
2024-05-10 14:41:08 +0100

    Updated logger and added StaticString extension.

--- Sources/RexselKernel/Extensions/Extensions+String.swift
+++ Sources/RexselKernel/Extensions/Extensions+String.swift
@@ -27,6 +27,12 @@ extension String {
 
 extension String {
 
+    init(_ staticString: StaticString) {
+        self = staticString.withUTF8Buffer {
+            String(decoding: $0, as: UTF8.self)
+        }
+    }
+
 #if os(macOS)
     var isValidURL: Bool {
         get {
