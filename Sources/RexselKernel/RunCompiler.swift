cc25c3c229f68e1e42965c608d08eb3371ac804c
Hugh Field-Richards <hsfr@hsfr.org.uk>
2024-08-14 11:11:58 +0100

    Temporary hold (symbol table problem)

---
+++ Sources/RexselKernel/RunCompiler.swift
@@ -0,0 +1,18 @@
+//
+//  RunCompiler.swift
+//  Rexsel
+//
+//  Created by Hugh Field-Richards on 14/08/2024.
+//
+
+import Foundation
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+class RunAssembler: AsynchronousOperation {
+
+
+
+
+}
